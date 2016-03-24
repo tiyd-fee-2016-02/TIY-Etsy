a95b2c7dda30034d1a287232443670e01c4d4dfa
Tori Hedden <tori.hedden@gmail.com>
2016-03-24 16:08:14 -0400

    add ng-repeat tags, attempt at cycling thru carousel

--- Assets/JS/listingPageController.js
+++ Assets/JS/listingPageController.js
@@ -1,31 +1,49 @@
 var listingPageApp = angular.module('listingPageApp', ['ngRoute']);
 
     var listingID = 272177332;
+    var gListing;
+    var gImages;
 
 listingPageApp.controller('listingPageController', ['$http', '$scope', function($http, $scope) {
     console.log("this works?")
-    // create a message to display in our view
-    $scope.message = 'THIS WORKS, I HOPE';
-
 
 
     $http.get('https://openapi.etsy.com/v2/listings/'+ listingID +'?api_key=s0og6fu8wnro0qfl4roi1muj').success(function(data){
       // console.log(data);
       $scope.listing = data.results[0];//just display the first one for now
+      $scope.tags = data.results[0].tags
+      gListing = $scope.listing
 
     })
 
     $http.get('https://openapi.etsy.com/v2/listings/'+ listingID +'/images?api_key=s0og6fu8wnro0qfl4roi1muj').success(function(data){
       $scope.images = data.results;//just display the first one for now
-      console.log(data);
+      // console.log(data);
+      gImages = $scope.images
 
     })
+}]);//end listing controller
 
 
-    // $http.get('https://openapi.etsy.com/v2/shops/6509277/listings/active?api_key=s0og6fu8wnro0qfl4roi1muj').success(function(data){
-    //   console.log(data);
-    //   $scope.store = data.results[0];//just display the first one for now
-    // })
+    listingPageApp.controller("carouselCtrl", function($scope){
+      $scope.carouselClick = 0;
+      console.log("carousel ctroller is up")
 
+      $scope.carouselClicker = function(carouselClick){
+        var tempClick = carouselClick;
 
-  }]);//end controller
+        if (tempClick > 3){
+
+          tempClick = 0;
+        }
+        else if (tempClick < 0){
+          tempClick = 3;
+        }
+        else {
+          //do nothing
+        }
+        console.log(tempClick);
+
+        return tempClick;
+      }
+    });
