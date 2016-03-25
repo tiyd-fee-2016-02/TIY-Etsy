c4a60bfdaab6a7e63c419de1ccd0026fd228cffa
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-03-25 13:18:54 -0400

    hope these variables work

--- Assets/JS/controllers.js
+++ Assets/JS/controllers.js
@@ -1,13 +1,12 @@
 // moreTrending();
 
-var listingID = '';
+var listingID;
 
 etsyApp.controller('etsyController', ['$scope', '$http', function($scope, $http){
  $scope.message = "hello world";
 
  $http.get("https://openapi.etsy.com/v2/listings/trending/?api_key=xkpd25inafnxzw5h0dmyo8ko&includes=MainImage,Shop&limit=100&offset=50").then(function(response){
    $scope.trending = response.data;
-
 
   //  var card = response.data;
    console.log(response.data);
@@ -26,6 +25,7 @@ etsyApp.controller('etsyController', ['$scope', '$http', function($scope, $http)
      function truncate(title, max_chars){
        return (title.length > max_chars) ? title.substring(0, max_chars) + '...' : title;
      };
+
     //  SOURCE: http://stackoverflow.com/questions/5074759/how-do-i-limit-the-length-of-an-link
 
      for(var i = start; i <= end; i+=4){
@@ -38,6 +38,7 @@ etsyApp.controller('etsyController', ['$scope', '$http', function($scope, $http)
        console.log(image);
        console.log(truncate(title,max_chars));
        console.log(listingID);
+
      $("#trending-grid-col-one").append('<div class = "trending-card-container"><a href = "" class = "trending-card-image"> <img src="'+image+'"></img></a><a href = "" class = "trending-card-title">'+title+'</a><a href = "" class = "trending-card-seller">'+shopName+'</a><div class = "trending-card-price">$'+price+'</div>');
      }
       for(var i = start+1; i <= end; i+=4){
@@ -47,10 +48,8 @@ etsyApp.controller('etsyController', ['$scope', '$http', function($scope, $http)
         var price = $scope.trending.results[i].price;
         var listID = $scope.trending.results[i].listing_id;
         listingID = listID;
-        // var max_chars = 25;
-        // function truncate(title, max_chars){
-        //   return (title.length > max_chars) ? title.substring(0, max_chars) + '..' : title;
-        // };
+        console.log(listingID);
+
      $("#trending-grid-col-two").append('<div class = "trending-card-container"><a href = "" class = "trending-card-image"> <img src="'+image+'"></img></a><a href = "" class = "trending-card-title">'+title+'</a><a href = "" class = "trending-card-seller">'+shopName+'</a><div class = "trending-card-price">$'+price+'</div>');
      }
       for(var i = start+2; i <= end; i+=4){
@@ -60,10 +59,8 @@ etsyApp.controller('etsyController', ['$scope', '$http', function($scope, $http)
         var price = $scope.trending.results[i].price;
         var listID = $scope.trending.results[i].listing_id;
         listingID = listID;
-        // var max_chars = 25;
-        // function truncate(title, max_chars){
-        //   return (title.length > max_chars) ? title.substring(0, max_chars) + '..' : title;
-        // };
+        console.log(listingID);
+
      $("#trending-grid-col-three").append('<div class = "trending-card-container"><a href = "" class = "trending-card-image"> <img src="'+image+'"></img></a><a href = "" class = "trending-card-title">'+title+'</a><a href = "" class = "trending-card-seller">'+shopName+'</a><div class = "trending-card-price">$'+price+'</div>');      }
       for(var i = start+3; i <= end; i+=4){
         var image = $scope.trending.results[i].MainImage.url_570xN;
@@ -72,10 +69,8 @@ etsyApp.controller('etsyController', ['$scope', '$http', function($scope, $http)
         var price = $scope.trending.results[i].price;
         var listID = $scope.trending.results[i].listing_id;
         listingID = listID;
-        // var max_chars = 25;
-        // function truncate(title, max_chars){
-        //   return (title.length > max_chars) ? title.substring(0, max_chars) + '..' : title;
-        // };
+        console.log(listingID);
+
      $("#trending-grid-col-four").append('<div class = "trending-card-container"><a href = "" class = "trending-card-image"> <img src="'+image+'"></img></a><a href = "" class = "trending-card-title">'+title+'</a><a href = "" class = "trending-card-seller">'+shopName+'</a><div class = "trending-card-price">$'+price+'</div>');
      }
      start = start + 16;
@@ -83,6 +78,7 @@ etsyApp.controller('etsyController', ['$scope', '$http', function($scope, $http)
      console.log(start);
      console.log(end);
      };//closes moreTrending
+
 
      $scope.moreTrending();
 
