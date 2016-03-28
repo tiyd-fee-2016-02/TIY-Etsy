53fe8b029bc1d0d24f7477c50b56aa48b0a45ef5
Jeremy Smith <smithjeremym@gmail.com>
2016-03-28 10:52:59 -0400

    uncomment product page routing in app.js

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -4,11 +4,10 @@ app.config(function($routeProvider) {
   $routeProvider
     .when('/home', {
       templateUrl: 'pages/home.html'
-      // controller: 'homepageController'
     })
-    // .when('/product', {
-    //   templateUrl: 'productPage.html'
-    // })
+    .when('/product', {
+      templateUrl: 'productPage.html'
+    })
     .otherwise({
       redirectTo: '/home'
     });
